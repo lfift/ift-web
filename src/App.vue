f26c4947bde13b9d699cf1bb3caed5721651b5cf
ift <1987047448@qq.com>
2018-09-26 22:39:09 +0800

    修改登录页面

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+
     <router-view/>
   </div>
 </template>
@@ -12,12 +12,16 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#app{
+  border: thin solid black;
+  height: 100%;
+}
+html,body{
+  margin: 0;
+  border: 0;
+  outline: none;
+}
+*{
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/Login.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="loginPage">
+    <el-tabs v-model="activeName" @tab-click="handleClick" >
+      <el-tab-pane label="登录" name="login">用户管理</el-tab-pane>
+      <el-tab-pane label="注册" name="regist">配置管理</el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      activeName: 'login'
+    }
+  },
+  methods: {
+    handleClick(tab,event) {
+      console.log(tab,event);
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+#loginPage{
+  width: 450px;
+  height: 350px;
+  border: thin solid red;
+  position:fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+}
+.el-tabs__header{
+  font-size: 20em;
+}
+</style>
